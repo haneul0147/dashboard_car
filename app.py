388554d71ac15118a63b5c25b223a790c2506cc8
haneul0147 <96038759+haneul0147@users.noreply.github.com>
2021-12-15 12:15:22 +0900

    frist

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import numpy as np
+import os
+import pickle
+
+from eda_app import run_eda_app
+
+def main():
+    st.title('자동차 가격 예측')
+
+    # 사이드바 메뉴
+    menu = ['home','EDA','ML']
+    choice=st.sidebar.selectbox('메뉴',menu)
+
+    if choice == 'home':
+        st.write('이 앱은 고객데이터와 자동차 구매 데이터에 대한 관한 정보입니다. 해당 고객에 정보를 입력하면 자동차를 예측해줍니다')
+        st.write('왼쪽의 사이드바에서 선택하세요')
+    elif choice == 'EDA':
+        run_eda_app()
+
+if __name__ == '__main__':
+    main()
