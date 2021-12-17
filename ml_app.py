8703368faaf226c31ef0c48075042e1272235ebf
haneul0147 <96038759+haneul0147@users.noreply.github.com>
2021-12-17 12:33:40 +0900

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -10,7 +10,7 @@ def run_ml_app():
     gender=st.radio('성별을 입력하세요.',['남자','여자'])
     if gender == '남자':
         gender_number = 1
-    elif gender ==' 여자':
+    elif gender =='여자':
         gender_number = 0
 
     age=st.number_input('나이 입력',min_value=1,max_value=100)
