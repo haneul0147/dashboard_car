c95c5b62a4c3fde6e1ff8ceae5331a2bef63240f
haneul0147 <96038759+haneul0147@users.noreply.github.com>
2021-12-24 18:09:36 +0900

    update

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ from eda_app import run_eda_app
 from ml_app import run_ml_app
 
 def main():
-    st.title('자동차 가격 예측')
 
     # 사이드바 메뉴
     menu = ['home','EDA','ML']
     choice=st.sidebar.selectbox('메뉴',menu)
 
     if choice == 'home':
-        st.write('이 앱은 고객데이터와 자동차 구매 데이터에 대한 관한 정보입니다. 해당 고객에 정보를 입력하면 자동차를 예측해줍니다')
-        st.write('왼쪽의 사이드바에서 선택하세요')
+        st.title('would you like car??')
+        if st.button('Click here') :
+            st.video('https://youtu.be/T8YOJIoOifc',format='video/mp4')
+    
     elif choice == 'EDA':
         run_eda_app()
     elif choice == 'ML':
